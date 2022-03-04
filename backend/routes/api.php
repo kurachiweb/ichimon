8a699fb6954d11f6a2dfef749a396e75b19612a1
倉地俊輔 <sukuratti3408@gmail.com>
2022-03-05 00:06:11 +0900

    backend ユーザー用CRUD雛形を作成

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::group(['middleware' => ['auth:sanctum']], function () {
+//     Route::apiResource('/users', UserController::class);
+// });
+Route::apiResource('/users', 'App\Http\Controllers\UserController');
