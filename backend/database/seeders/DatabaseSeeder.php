8a8576bfd1b28122a5f85e440e5318df0be56388
倉地俊輔 <sukuratti3408@gmail.com>
2022-03-08 00:00:15 +0900

    backend  seeder実行時にtruncateでデータ削除

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
@@ -11,6 +12,12 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
+        // truncate時、外部キー制約の検証を無効にする
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('user')->truncate();
+        DB::table('user_auth')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
         $this->call([
             UserTableSeeder::class,
             UserAuthSeeder::class
