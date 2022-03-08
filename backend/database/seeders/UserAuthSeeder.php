77cbeda317510ff2b4fd133e8f25916fba47f418
倉地俊輔 <sukuratti3408@gmail.com>
2022-03-08 23:45:24 +0900

    backend seeder 実際に暗号化/ハッシュ化した文字列を投入

--- backend/database/seeders/UserAuthSeeder.php
+++ backend/database/seeders/UserAuthSeeder.php
@@ -22,13 +22,21 @@ class UserAuthSeeder extends Seeder {
         $registered_at = new DateTime();
         $registered_at->setDate(2016, 4, 5)->setTime(22, 0, 47);
 
+        /** testmailaddress@supercalifragilisticexpialidocioustestmailaddresssupercalifragilisticexpialidocious.supercalifragilisticexpialidocioustestmailaddresssupercalifragilisticexpialidocious_example-supercalifragilisticexpialidocioustestmailaddresssupercalifr.com をaes-256-cbcで暗号化した文字列及びsha3-512でハッシュ化した文字列 */
+        $encrypted_email = 'eyJpdiI6IjNVUWtib084QVI2bzlLbnRVVklrSXc9PSIsInZhbHVlIjoiaTNoNVkycTRRS0dnamo2ZTlGTWZxdHJRaEdEa0o5aEJ5SGVFTytoY28za2NNcHpYUFlVWHFQamF2MjFKMWxZT0g4OHdMUDlSZW9oSFhtN0YybVVpejVCMmd5UVhkR2t4MnVUS2Y5bytMc0taeUVoTDg2ZFVoRFFBSjluK205eGZ2RG1oR1pFVWlGUFdveTZwckU5a3BnV0VpSVZYT2Frd3l1RlEvcDNNellkV093OG5rL2ROeit2YlN1czQyQ2RnRkZuMGN2VndWOWFRRXBVUGVqTGt5Umh5NW51dUV1bEpWRVhod2FpL0VqSVpvQXY4d25lWjJsbllyQ1d3ejBoSDg2V1ROV3p2RTV0VDdiMU1PTk1uakJ2OTAxZHRCSTZ1YllhamN1MWxXbEVSWlkxZUo0NjJqbEpoTHp0ajUxWUIxMG81OW9BV0pRazdFZGlPdkd3b1RpQ1ZrVFlPVFE5bFpyZnZyN0toc2k4PSIsIm1hYyI6IjFjNDhmMGY0M2MzNDEyMjQwNzU3ZjRhYTVkOGUwYTBkMDc2MDI0NTRjNjVmYTVjMDFkZDdiNTNmNTAzN2M5YzMiLCJ0YWciOiIifQ==';
+        /** password_hash_challenge をsha3-512でハッシュ化した文字列 */
+        $hashed_email = '57cee59b269049126ac9973af4c7daac04f408d4ee0fecfab340b3ca704dda675d4600bedb7657542ff927a2d2f69eb6e91a18503461882432e4f70fc826ef65';
+        /** password_hash_challenge をsha3-512でハッシュ化した文字列 */
+        $hashed_password = '358b0b50c4a5859323c2f020157781fdb0a2d7cdda8227160e7eecbc5b689f40c5a04564ad93f9533e433ebd707bb16a059322b1cb16fd88e376c33f175db67c';
+
         DB::table('user_auth')->insert([
             [
                 'id' => 345678912,
                 'user_id' => $user_id,
-                'email' => 'eyJpdiI6Im0xSndNYmt2NWJpV0Y0Nmo5enozRVE9PSIsInZhbHVlIjoiTE9UVHZnMGhsYXNvM25xSldXamFVU2s0TDVwVk5VQXlsR1BRVFpaRWU2S041bXNlVkIxQXBNTEVEdXowejk1cEtBRUxYSjBlUG14c21oOTlzODJ3Y1B1YnNvOVRMeUhLRkVIdGw2b1I2RHdoWEY2MjIvVWtrT1ZnVEdCUlBFN1JFdVJuWmwrc0VlV25UNGkxc0psS3Z5R0pkVldhOHUrSDg5eWx4VDFGWTh6VmVMd3Rjc1VrelpRKy9iT1lSQW8yK0c0MXg3ZVRkdlhNUFFqdUoyRXlCZVY0VE5wcjJiTzVEV01ZSkY3YlM2b0FMNUpKOXJxRVJCMUJuUDJ3aGZDMy9zWVA5dS9aQ29XUE1ObHVYZFZPSW5NOER5T0xBNWg3a2V5eG9Vd2FmcjVPTWh0OFEwRDVCMGwxcERQbGhqenVtT096cnZOUXhsbEF3eHlFcy80YTNtYmRsWThEc09nQWJ3NzVQdXkreWhRPSIsIm1hYyI6ImY5NDIxNmU4ZWY2Nzg1OGI4MzRiNDA4NTFlYjUxZGQ4Y2NlMmE4NWNmY2M1MWFmMWNmMjVkZGZmZTE0NGNjYjkiLCJ0YWciOiIifQ==',
+                'email' => $encrypted_email,
+                'email_hash' => $hashed_email,
                 // 'email_alter' => null,
-                'password' => 'eyJpdiI6InFqWkhNTHg4cHNBUFdkcklyUlJia3c9PSIsInZhbHVlIjoiVWxBNEsrUXBIODFicjZSY1VWUlQzeStIYTlOQ0FjeXB2RlQyRSswUUErQT0iLCJtYWMiOiI4ZDBmYTZkYTI5MWMxOGVkOTU1MWFkN2RjOGM2NWQyODBkMzZlMmI0ZDkwYTA5NzQ1ZjllY2I0MmUzM2MxNWVmIiwidGFnIjoiIn0',
+                'password' => $hashed_password,
                 'subscription_id' => 'sub_hogehoge',
                 'created_at' => $created_at,
                 'updated_at' => $registered_at,
