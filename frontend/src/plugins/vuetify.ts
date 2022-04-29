47a6faf00616bd6430ce6802d482c705c8f7ed0a
倉地俊輔 <sukuratti3408@gmail.com>
2022-04-29 15:08:18 +0900

    frontend プライマリ色を水色#28c3d5に

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -7,7 +7,7 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: '#1976D2',
+        primary: '#28c3d5',
         secondary: '#424242',
         accent: '#82B1FF',
         error: '#FF5252',
