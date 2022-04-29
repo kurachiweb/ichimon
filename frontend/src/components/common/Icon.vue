57e874e9c8e222f755e80e216a985165d41deca0
倉地俊輔 <sukuratti3408@gmail.com>
2022-04-29 11:57:26 +0900

    frontend SVGアイコンコンポーネントを作成

---
+++ frontend/src/components/common/Icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <CommonSvg :path="singlePath" :viewBox="viewBox" />
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { ICON } from '@/components/types/icons';
+import CommonSvg from '@/components/common/Svg.vue';
+
+@Component({
+  components: {
+    CommonSvg
+  }
+})
+export default class CommonIcon extends Vue {
+  @Prop() name!: keyof typeof ICON; // アイコン名
+  @Prop({ default: '0 0 512 512' }) viewBox!: string; // SVGの仮想描画サイズ
+
+  /** アイコン名から単一パス属性値に */
+  private get singlePath(): string {
+    return ICON[this.name] ?? '';
+  }
+}
+</script>
