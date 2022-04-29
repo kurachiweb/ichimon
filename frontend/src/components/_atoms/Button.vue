3b9e92cd2ec29f807d37baf29e4836a0cb6d095d
倉地俊輔 <sukuratti3408@gmail.com>
2022-04-29 18:31:35 +0900

    frontend 共通ボタンコンポーネントを作成

---
+++ frontend/src/components/_atoms/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn :color="color">
+    <slot />
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class CommonButton extends Vue {
+  @Prop({ default: 'primary darken-2' }) color?: string; // ボタンのテーマ色
+}
+</script>
