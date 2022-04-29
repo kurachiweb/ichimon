f50eea7be7277c1d3bb378784e5e7317a891c08e
倉地俊輔 <sukuratti3408@gmail.com>
2022-04-29 15:49:45 +0900

    frontend デフォルトのタイトルと説明文をオリジナルに

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,5 +3,12 @@ module.exports = defineConfig({
   transpileDependencies: ['vuetify'],
   devServer: {
     port: 55001
+  },
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: '一問',
+      description: 'Webアンケートサービスの、一問です。忙しい日々の合間に、10秒で回答できます。'
+    }
   }
 });
