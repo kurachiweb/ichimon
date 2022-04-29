dda87c1b5272d506f716e8662058dbb41506d31d
倉地俊輔 <sukuratti3408@gmail.com>
2022-04-29 11:56:27 +0900

    frontend  SVG要素のラッパーコンポーネントを作成

---
+++ frontend/src/components/common/Svg.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg>
+    <slot />
+    <path v-if="path" :d="path" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class CommonSvg extends Vue {
+  @Prop() path?: string; // 単一パス属性値
+}
+</script>
