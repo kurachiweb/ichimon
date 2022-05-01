c6bb14953460d542cac53f8384761fd854e9fc28
倉地俊輔 <sukuratti3408@gmail.com>
2022-05-01 19:29:47 +0900

    frontend アカウント作成 送信ボタンで作成リクエストを送信

--- frontend/src/components/account/Create.vue
+++ frontend/src/components/account/Create.vue
@@ -1,15 +1,29 @@
 <template>
-  <v-form>
-    <CommonTextField label="ユーザーID" autocomplete="username" />
-    <CommonTextField label="メールアドレス" type="email" autocomplete="email" />
-    <CommonTextField label="パスワード" type="password" autocomplete="new-password" />
+  <v-form @submit.prevent="requestCreate()">
+    <CommonTextField v-model="account.display_id" label="ユーザーID" autocomplete="username" />
+    <CommonTextField v-model="account.auth.email" label="メールアドレス" type="email" autocomplete="email" />
+    <CommonTextField v-model="account.auth.password" label="パスワード" type="password" autocomplete="new-password" />
     <CommonButton type="submit">この内容で登録する</CommonButton>
   </v-form>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { DefaultAccount } from '@/models/account/account';
 
 @Component
-export default class AccountCreate extends Vue {}
+export default class AccountCreate extends Vue {
+  private account = DefaultAccount();
+
+  /** アカウント作成リクエストを送信 */
+  private requestCreate() {
+    fetch('http://127.0.0.1:55002/api/users', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(this.account)
+    })
+      .then(r => r.json())
+      .then(console.log);
+  }
+}
 </script>
