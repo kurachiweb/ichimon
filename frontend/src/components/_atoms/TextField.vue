0d638eb387dd2f373e878445f66c2553a9300385
倉地俊輔 <sukuratti3408@gmail.com>
2022-05-01 19:25:00 +0900

    frontend 共通テキスト入力 v-modelが動作するように

--- frontend/src/components/_atoms/TextField.vue
+++ frontend/src/components/_atoms/TextField.vue
@@ -1,12 +1,26 @@
 <template>
-  <v-text-field v-bind="$attrs">
+  <v-text-field v-bind="$attrs" v-model="Value">
     <slot />
   </v-text-field>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
 
 @Component
-export default class CommonTextField extends Vue {}
+export default class CommonTextField extends Vue {
+  @Prop() value!: string; // 入力値
+
+  @Emit()
+  private input(newValue: string) {
+    return newValue;
+  }
+
+  private get Value() {
+    return this.value;
+  }
+  private set Value(newValue: string) {
+    this.input(newValue);
+  }
+}
 </script>
