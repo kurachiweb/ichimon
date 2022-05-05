c9ed20ffdee7bbd1d404cbe589961d4cc09b2df5
倉地俊輔 <sukuratti3408@gmail.com>
2022-05-05 23:52:52 +0900

    backend メール認証トークンテーブルに対応するモデルを作成

---
+++ backend/app/Models/VerifyEmailToken.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VerifyEmailToken extends Model {
+    use HasFactory;
+
+    /**
+     * テーブル名
+     *
+     * @var string
+     */
+    protected $table = 'verify_email_token';
+
+    /**
+     * IDはオートインクリメントか
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * プライマリキーのカラム名
+     *
+     * @var string
+     */
+    protected $primaryKey = 'token';
+
+    /**
+     * プライマリキーの型
+     *
+     * @var bool
+     */
+    protected $keyType = 'string';
+
+    /**
+     * 追加できない列
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = [];
+
+    /**
+     * 取得できない列
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * 取得/更新時に型を変換する
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'account_id' => 'int',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime'
+    ];
+
+    /**
+     * モデルのデフォルト値
+     * テーブルカラム・リレーション設定と合わせる
+     *
+     * @param boolean $relation - リレーション先のデータも併せて設定するか
+     * @return array<string, any>
+     */
+    public static function getDefault($relation = false) {
+        $model = [
+            'token' => '',
+            'account_id' => 0
+        ];
+        return $model;
+    }
+}
