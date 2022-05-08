e47410900bf654b9ee0ce923f16339e503528c75
倉地俊輔 <sukuratti3408@gmail.com>
2022-05-09 08:21:01 +0900

    frontend アカウントを作成したら即ログインを試みる

--- frontend/src/components/account/Create.vue
+++ frontend/src/components/account/Create.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-form @submit.prevent="requestCreate()">
+  <v-form @submit.prevent="onSubmitCreate()">
     <CommonTextField v-model="account.display_id" label="アカウントID" autocomplete="username" />
     <CommonTextField v-model="account.auth.email" label="メールアドレス" type="email" autocomplete="email" />
     <CommonTextField v-model="account.auth.password" label="パスワード" type="password" autocomplete="new-password" />
@@ -15,25 +15,62 @@ import { Account, DefaultAccount } from '@/models/account/account';
 export default class AccountCreate extends Vue {
   private account = DefaultAccount(true);
 
+  /** Cookieからキー指定で値を取得する */
+  private getCookie(key: string): string | null {
+    const cookies = document.cookie.split(',');
+    for (const cookie of cookies) {
+      const keyValue = cookie.split('=');
+      if (keyValue[0] === key) {
+        return keyValue[1];
+      }
+    }
+    return null;
+  }
+
+  /** 送信ボタンのクリック後 */
+  private onSubmitCreate() {
+    this.requestAccountCreate(this.account).then((account: Account) => {
+      this.requestAccountEmailVerify(account);
+      this.requestAccountLogin(account);
+      this.$router.push({ name: 'Home' });
+    });
+  }
+
   /** アカウント作成リクエストを送信 */
-  private requestCreate() {
-    fetch('http://127.0.0.1:55002/api/accounts', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(this.account)
-    })
-      .then(r => r.json())
-      .then(res => {
-        this.requestVerifyEmail(res.data.account);
-        this.$router.push({ name: 'Home' });
-      });
+  private requestAccountCreate(account: Account): Promise<Account> {
+    return new Promise(resolve => {
+      fetch('http://127.0.0.1:55002/api/accounts', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(account)
+      })
+        .then(r => r.json())
+        .then(res => {
+          resolve(res.data.account);
+        });
+    });
   }
 
   /** アカウントのメールアドレス認証リクエストを送信 */
-  private requestVerifyEmail(account: Account) {
+  private requestAccountEmailVerify(account: Account) {
     fetch('http://127.0.0.1:55002/api/verify/email/send/' + account.id)
       .then(r => r.json())
       .then(console.log);
+  }
+
+  /** アカウントログインリクエストを送信 */
+  private requestAccountLogin(account: Account): Promise<Account> {
+    return new Promise(resolve => {
+      fetch('http://127.0.0.1:55002/api/accounts/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ account })
+      })
+        .then(r => r.json())
+        .then(res => {
+          resolve(res.data.account);
+        });
+    });
   }
 }
 </script>
