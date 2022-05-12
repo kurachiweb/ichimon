9fbfa6215fb97d1f0a79f012ddc786b97033361b
倉地俊輔 <sukuratti3408@gmail.com>
2022-05-12 23:54:39 +0900

    frontend ログイン画面 ログインリクエストできるように

--- frontend/src/components/account/Login.vue
+++ frontend/src/components/account/Login.vue
@@ -1,14 +1,46 @@
 <template>
-  <v-form>
-    <CommonTextField label="メールアドレス または アカウントID" autocomplete="email" />
-    <CommonTextField label="パスワード" type="password" autocomplete="current-password" />
+  <v-form @submit.prevent="onSubmitLogin()">
+    <CommonTextField v-model="accountName" label="メールアドレス または アカウントID" autocomplete="email" />
+    <CommonTextField v-model="accountPassword" label="パスワード" type="password" autocomplete="current-password" />
     <CommonButton type="submit">ログインする</CommonButton>
   </v-form>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { Account } from '@/models/account/account';
 
 @Component
-export default class AccountLogin extends Vue {}
+export default class AccountLogin extends Vue {
+  /** メールアドレスまたはアカウントID */
+  private accountName = '';
+  /** パスワード */
+  private accountPassword = '';
+
+  /** 送信ボタンのクリック後 */
+  private onSubmitLogin() {
+    this.requestAccountLogin(this.accountName, this.accountPassword).then(console.log);
+    // this.$router.push({ name: 'Home' });
+  }
+
+  /** アカウントログインリクエストを送信 */
+  private requestAccountLogin(name: string, password: string): Promise<Account> {
+    return new Promise(resolve => {
+      fetch('http://127.0.0.1:55002/api/accounts/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        mode: 'cors',
+        credentials: 'include',
+        body: JSON.stringify({
+          name: name,
+          password: password
+        })
+      })
+        .then(r => r.json())
+        .then(res => {
+          resolve(res.data.account);
+        });
+    });
+  }
+}
 </script>
