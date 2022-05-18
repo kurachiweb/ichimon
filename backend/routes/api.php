e34e594daec69e5b9a0b69228fff6f2846e4a992
倉地俊輔 <sukuratti3408@gmail.com>
2022-05-18 23:59:51 +0900

    backend APIルーティングの一部にログイン認証ミドルウェアを

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,11 +17,17 @@ use Illuminate\Support\Facades\Route;
 // Route::group(['middleware' => ['auth:sanctum']], function () {
 //     Route::apiResource('/users', UserController::class);
 // });
-// アカウント基礎情報・認証情報のCRUD
-Route::apiResource('/accounts', 'App\Http\Controllers\AccountController');
+// アカウント作成
+Route::apiResource('/accounts', 'App\Http\Controllers\AccountController', ['only' => ['store']]);
 // アカウントログイン
 Route::post('/accounts/login', 'App\Http\Controllers\AccountLoginController');
-// 認証メール送信
-Route::get('/verify/email/send/{id}', 'App\Http\Controllers\SendVerifyEmailController');
-// メール認証トークン照合
-Route::post('/verify/email/check', 'App\Http\Controllers\CheckEmailVerifyController');
+
+// ログイントークンが有効なら
+Route::group(['middleware' => ['auth.login:login']], function () {
+  // アカウント基礎情報・認証情報のCRUD
+  Route::apiResource('/accounts', 'App\Http\Controllers\AccountController', ['except' => ['store']]);
+  // 認証メール送信
+  Route::get('/verify/email/send/{id}', 'App\Http\Controllers\SendVerifyEmailController');
+  // メール認証トークン照合
+  Route::post('/verify/email/check', 'App\Http\Controllers\CheckEmailVerifyController');
+});
