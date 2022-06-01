1d092d55288ffb8fe585c3b89a497b7517b73743
倉地俊輔 <sukuratti3408@gmail.com>
2022-06-01 21:52:17 +0900

    frontend eslintrcのmax-len値は単なる数値ではない

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,14 +3,19 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended'
+  ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'max-len': 90
+    'max-len': ['error', { code: 90 }]
   },
   overrides: [
     {
