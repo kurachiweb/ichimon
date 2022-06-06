519a4e9ba1eb6ac91b5d36a906105d1f94aa75d1
Kurachi Shunsuke <kurachi@wapon.co.jp>
2022-06-06 18:48:33 +0900

    backend アカウントログイン認証されている場合のみ使えるAPIを選ぶ

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -24,9 +24,17 @@ Route::post('/accounts/login', 'App\Http\Controllers\AccountLoginController');
 // ログイン状態なら
 Route::group(['middleware' => ['account.auth:authz']], function () {
   // アカウント基礎情報・認証情報のRUD
-  Route::apiResource('/accounts', 'App\Http\Controllers\AccountController', ['except' => ['store']]);
-  // 認証メール送信
-  Route::post('/verify/email/send', 'App\Http\Controllers\SendVerifyEmailController');
-  // メール認証トークン照合
-  Route::post('/verify/email/check', 'App\Http\Controllers\CheckEmailVerifyController');
+  Route::apiResource('/accounts', 'App\Http\Controllers\AccountController', ['only' => ['index']]);
+
+  // ログインユーザーを認証できれば
+  Route::group(['middleware' => ['account.auth:authr']], function () {
+    // アカウント基礎情報・認証情報のRUD
+    Route::apiResource('/accounts', 'App\Http\Controllers\AccountController', ['except' => ['index', 'store']]);
+
+    // 認証メール送信
+    Route::post('/verify/email/send', 'App\Http\Controllers\SendVerifyEmailController');
+
+    // メール認証トークン照合
+    Route::post('/verify/email/check', 'App\Http\Controllers\CheckEmailVerifyController');
+  });
 });
