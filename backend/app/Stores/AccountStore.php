fb992da741ca8fec8858e252f24c2cb1d7087a7d
倉地俊輔 <sukuratti3408@gmail.com>
2022-07-10 11:15:27 +0900

    backend ストアはセッションではなくRedisを利用

--- backend/app/Stores/AccountStore.php
+++ backend/app/Stores/AccountStore.php
@@ -4,35 +4,45 @@ declare(strict_types=1);
 
 namespace App\Stores;
 
-use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Redis;
 
 use App\UseCases\Account\AccountGetCase;
 
 class AccountStore {
     /**
-     * セッションからアカウント情報を取得
+     * 保存領域からアカウント情報を取得
      *
      * @param string $account_id
      * @return array|null
      */
     public static function get($account_id) {
-        // セッションに保存されたアカウント情報を取り出す
-        return Session::get('account' . $account_id);
+        $redis = Redis::connection('account');
+        
+        $account = $redis->get($account_id);
+
+        // 取得データが文字でない場合は、decodeできないためそのまま返す
+        if (!is_string($account)) {
+            return $account;
+        }
+        return json_decode($account, true);
     }
 
     /**
-     * セッションにアカウント情報を保存
+     * 保存領域にアカウント情報を保存
      *
      * @param string $account_id
      * @return array
      */
     public static function save($account_id) {
+        $redis = Redis::connection('account');
+
         // DBにアクセスしてアカウント情報を取得する
         $accountGetCase = new AccountGetCase();
         $account = $accountGetCase($account_id, true)->toArray();
 
-        // このリクエスト限りのセッションに保存
-        Session::now('account' . $account_id, $account);
+        // アカウント情報用キャッシュに接続し、保存する
+        // Redisには配列をそのまま入れられないため文字列化
+        $redis->set($account_id, json_encode($account));
 
         return $account;
     }
