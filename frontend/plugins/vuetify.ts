0e2fd0c8804c43a256bb15d6fc224c7a98233ab8
倉地俊輔 <sukuratti3408@gmail.com>
2022-08-01 23:07:28 +0900

    frontend テーマカラーを定義

--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,12 +1,42 @@
-import { createVuetify } from 'vuetify';
+import { Vuetify } from 'nuxt/dist/app/compat/capi';
+import { createVuetify, ThemeDefinition } from 'vuetify';
 import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives'
+import * as directives from 'vuetify/directives';
 import '@mdi/font/scss/materialdesignicons.scss';
+
+const themeLight: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    text: '#2B2C03',
+    primary: '#FBFF3C',
+    'primary-darken-1': '#D9DD2B',
+    secondary: '#03DAC6',
+    'secondary-darken-1': '#05bfae',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00'
+  }
+};
+
+/** Vuetifyの現在のテーマ定義から色を取得 */
+export const vCurrentTheme = (instance: Vuetify, colorKey: string) => {
+  const current = unref(instance.theme.current);
+  return current.colors[colorKey];
+};
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     components,
-    directives
+    directives,
+    theme: {
+      defaultTheme: 'light',
+      themes: {
+        light: themeLight
+      }
+    }
   });
 
   nuxtApp.vueApp.use(vuetify);
