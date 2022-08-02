28c11a9830f885f272ca7078ecd37c4b59ce5b78
倉地俊輔 <sukuratti3408@gmail.com>
2022-08-02 21:47:06 +0900

    frontend ヘッダーロゴ $vuetifyから現在のテーマに基づく色を取得

--- frontend/components/base/header.vue
+++ frontend/components/base/header.vue
@@ -1,10 +1,16 @@
 <template>
   <v-app-bar app color="primary" class="w-100">
     <NuxtLink :to="{ name: 'index' }">
-      <CommonIcon name="logo" height="32" fill="white" />
+      <CommonIcon name="logo" height="32" :fill="currentColor($vuetify, 'text')" />
     </NuxtLink>
   </v-app-bar>
 </template>
+
+<script lang="ts" setup>
+import { vCurrentTheme } from '~/plugins/vuetify';
+
+const currentColor = computed(() => vCurrentTheme);
+</script>
 
 <script lang="ts">
 export default defineNuxtComponent({
