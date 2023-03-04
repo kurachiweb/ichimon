9ab97f0c05090e2cb89b009144b94a2fc8fc6147
Kurachi Shunsuke <kurachiweb@gmail.com>
2023-03-04 13:27:13 +0900

    backend ログDBのDB/テーブル/カラム名を定数化

---
+++ backend/app/Constants/Db/Log/DbTableLogAccountSecurity.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Constants\Db\Log;
+
+use App\Constants\Db\DbName;
+use App\Constants\Db\DbTableLogCommon;
+use App\Constants\Db\DbTableName;
+
+/**
+ * 定数: アカウントのセキュリティログ
+ */
+class DbTableLogAccountSecurity extends DbTableLogCommon {
+  /** アカウントのセキュリティログ: DB名 */
+  const DB_NAME = DbName::LOG;
+  /** アカウントのセキュリティログ: テーブル名 */
+  const TABLE_NAME = DbTableName::LOG_ACCOUNT_SECURITY;
+
+  /** アカウントのセキュリティログ: 対象のアカウント基本ID */
+  const ACCOUNT_ID = 'account_id';
+}
