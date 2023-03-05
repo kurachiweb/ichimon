dd40a6fbbc32bfeed9b79ba7b77f5142bb20b3fd
Kurachi Shunsuke <kurachiweb@gmail.com>
2023-03-05 11:49:20 +0900

    backend DB接続設定 複数DBに対応

--- backend/config/database.php
+++ backend/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mariadb_account'),
 
     /*
     |--------------------------------------------------------------------------
@@ -43,25 +43,46 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_bin',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+        ...(function () {
+            $db_connect_default = [
+                'driver' => 'mysql',
+                'url' => env('DATABASE_URL'),
+                'host' => env('DB_HOST', '127.0.0.1'),
+                'port' => env('DB_PORT', '3306'),
+                'database' => env('DB_DATABASE', 'forge'),
+                'username' => env('DB_USERNAME', 'forge'),
+                'password' => env('DB_PASSWORD', ''),
+                'unix_socket' => env('DB_SOCKET', ''),
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_bin',
+                'prefx' => '',
+                'prefix_indexes' => true,
+                'strict' => true,
+                'engine' => null,
+                'options' => extension_loaded('pdo_mysql') ? array_filter([
+                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                ]) : [],
+            ];
+
+            return [
+                'db_account' => [
+                    ...$db_connect_default,
+                    'database' => env('DB_DATABASE_ACCOUNT', 'account'),
+                ],
+                'db_survey' => [
+                    ...$db_connect_default,
+                    'database' => env('DB_CONNECTION_SURVEY', 'survey'),
+                ],
+                'db_token' => [
+                    ...$db_connect_default,
+                    'database' => env('DB_CONNECTION_TOKEN', 'token'),
+                ],
+                'db_log' => [
+                    ...$db_connect_default,
+                    'database' => env('DB_CONNECTION_LOG', 'log'),
+                ],
+            ];
+        })(),
 
         'pgsql' => [
             'driver' => 'pgsql',
